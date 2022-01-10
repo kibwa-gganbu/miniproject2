350cf536cffa1e572f9300763a55d39fe32a244d
sunnyl94 <seonillee94@gmail.com>
2022-01-10 15:27:28 +0900

    updated mongo_mysql.sh

--- mongo_mysql.sh
+++ mongo_mysql.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
+mongo < find_avg.sql
+
 mongoexport -d test -c parking_avg -f floor,date,hour,avg_num --type=csv -o /Users/seonil/Desktop/workspace/miniproject2/parking.csv
 
 db='mysql --local_infile=1 -u root -pQlalf679498 --database=gongju'
